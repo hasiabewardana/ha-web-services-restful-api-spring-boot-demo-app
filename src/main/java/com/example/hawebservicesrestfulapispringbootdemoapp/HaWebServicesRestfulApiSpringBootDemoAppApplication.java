b48c4d944f094a6aadbee7d9b9ba7eeaa22702d7
hasiabewardana <hasi.prasanga@gmail.com>
2022-12-02 14:57:15 +0530

    Started the Web Services and RESTful API project.

---
+++ src/main/java/com/example/hawebservicesrestfulapispringbootdemoapp/HaWebServicesRestfulApiSpringBootDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.hawebservicesrestfulapispringbootdemoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaWebServicesRestfulApiSpringBootDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaWebServicesRestfulApiSpringBootDemoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hawebservicesrestfulapispringbootdemoapp/HaWebServicesRestfulApiSpringBootDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hawebservicesrestfulapispringbootdemoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaWebServicesRestfulApiSpringBootDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
